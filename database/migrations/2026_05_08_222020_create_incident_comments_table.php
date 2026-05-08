69b4ee668fd928556b4c2981c94f06391fef2936
gywilmarroquin <gywilmarroquin@micoopeguayacan.gt>
2026-05-08 16:22:35 -0600

    Creación de la tabla para el control de los comentarios de los insidentes.

---
+++ database/migrations/2026_05_08_222020_create_incident_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('incident_comments', function (Blueprint $table) {
+            $table->id();
+
+            // Usamos text() porque los comentarios suelen ser largos y superar los 255 caracteres de un string()
+            $table->text('description');
+
+            // Llave foránea conectada a la tabla incidents
+            $table->foreignId('incident_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            // Crea automáticamente created_at y updated_at
+            $table->timestamps();
+
+            // Crea automáticamente la columna deleted_at para el Soft Deletion
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incident_comments');
+    }
+};
